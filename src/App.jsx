f92866c88b081690df41ac805606ac93de50bfd9
eslamgh86 <eslamgomaa1986@gmail.com>
2025-01-12 23:50:33 +0200

    vercel update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import {  createHashRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import Home from "./Components/Home/Home";
 import About from "./Components/About/About";
@@ -8,7 +8,7 @@ import Contact from "./Components/Contact/Contact";
 import Notfound from "./Components/Notfound/Notfound";
 
 
-let router = createHashRouter([
+let router = createBrowserRouter([
   {
     path: "",
     element: <Layout />,
