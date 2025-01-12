4090d8c83f68dcd0168f92b6a28de97a60c0d1b5
eslamgh86 <eslamgomaa1986@gmail.com>
2025-01-12 23:56:11 +0200

    last

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -21,7 +21,7 @@ export default function Templatename() {
           <h1 className="font-bold text-4xl mb-1 after:content-[''] after:bg-white after:w-24 after:h-1 after:absolute after:top-48 after:-translate-x-36 before:content-[''] before:bg-white before:w-24 before:h-1 before:absolute before:transform before:translate-x-10 before:translate-y-14">START FRAMEWORK</h1>
 
             <i className='fa-solid fa-star'></i>
-          <h2 className="mt-6">Graphic Artist - Web Designer - Illustrator</h2>
+          <h2 className="mt-6">Graphic Artist - Web Designer - Illustrator Eslam</h2>
         </div>
       </div>
     </>
